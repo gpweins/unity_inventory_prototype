0f248b24aadd2a5c4e7b97ce791b60247bded7ad
Gustavo P. Weinschutz <gpweins@gmail.com>
2014-10-23 15:44:53 -0200

    Added Item database scripts

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item {
+
+	public string name;
+	public int ID;
+	public string description;
+	public Texture2D icon;
+	public int power;
+	public int speed;
+	public ItemType type;
+	
+	public enum ItemType {
+		Consumable,
+		Equipable,
+		Quest
+	}
+
+}
