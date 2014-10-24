ac9a909bdd793d6b0395523a748d38d466ea9660
Gustavo P. Weinschutz <gpweins@gmail.com>
2014-10-24 21:31:55 -0200

    Added Tags class to manage tag names via constants
    Updated Tag Manager list

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour {
+
+	// A list of tag strings.
+	public const string player = "Player";
+	public const string gameController = "GameController";
+	public const string itemDatabase = "ItemDatabase";
+	public const string inventory = "Inventory";
+	public const string item = "Item";
+}
