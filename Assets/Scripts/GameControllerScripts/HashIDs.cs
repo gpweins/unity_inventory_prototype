914c4fc3e1b0999650949f804402d9cc2a4f7d0d
Gustavo P. Weinschutz <gpweins@gmail.com>
2014-10-24 21:33:35 -0200

    Added GameController prefab

---
+++ Assets/Scripts/GameControllerScripts/HashIDs.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HashIDs : MonoBehaviour {
+
+	public int deadBool;
+	public int shoutingBool;
+	public int sneakingBool;
+	public int speedFloat;
+
+	public int dyingState;
+	public int locomotionState;
+	public int sneakState;
+
+	public int shoutState;
+
+
+	void Awake()
+	{
+		deadBool = Animator.StringToHash("Dead");
+		deadBool = Animator.StringToHash("Shouting");
+		deadBool = Animator.StringToHash("Sneaking");
+		deadBool = Animator.StringToHash("Speed");
+
+		dyingState = Animator.StringToHash("Base Layer.Dying");
+		locomotionState = Animator.StringToHash("Base Layer.Locomotion");
+		sneakState = Animator.StringToHash("Base Layer.Sneak");
+
+		shoutState = Animator.StringToHash("Shouting.Shout");
+	}
+
+}
