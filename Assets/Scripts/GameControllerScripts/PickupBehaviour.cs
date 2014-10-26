6f96cbc955ee446582420c03ed4f03243b63aa86
Gustavo P. Weinschutz <gpweins@gmail.com>
2014-10-26 16:18:27 -0200

    Added pickup behaviour

---
+++ Assets/Scripts/GameControllerScripts/PickupBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupBehaviour : MonoBehaviour {
+
+	public string itemSlug;
+	public int quantity;
+
+	private GameObject player;
+	private Inventory inventory;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag(Tags.player);
+		inventory = GameObject.FindGameObjectWithTag(Tags.inventory).GetComponent<Inventory>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.Equals(player)) {
+			PickUp();
+		}
+
+	}
+
+	void PickUp()
+	{
+		inventory.Add(itemSlug, quantity);
+		Destroy(this.gameObject);
+	}
+}
